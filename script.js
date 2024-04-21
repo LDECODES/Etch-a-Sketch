7e4a27d9c29a8972cebec95bb02da94e366c08b1
LeonDEvans <evansleon87@gmail.com>
2024-04-20 21:08:23 -0400

    Revamped the Ui

---
+++ script.js
@@ -0,0 +1,18 @@
+gridSizeButton = document.querySelector('#gridSize')
+eraserButton = document.querySelector('#eraser')
+defaultButton = document.querySelector('#default')
+rainbowButton = document.querySelector('#rainbow')
+monochromeButton = document.querySelector('#monochrome')
+resetButton = document.querySelector('#resetGrid')
+
+function makeGrid( ) {
+
+}
+
+function makeRow() {
+
+}
+
+function makeCells() {
+
+}
