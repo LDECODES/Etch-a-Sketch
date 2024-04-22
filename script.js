61c136ed1465ad050ca87eee78aa70cb6c4ed83f
LeonDEvans <evansleon87@gmail.com>
2024-04-21 20:56:06 -0400

    Put buttons in variables

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ rainbowButton = document.querySelector('#rainbow')
 monochromeButton = document.querySelector('#monochrome')
 resetButton = document.querySelector('#resetGrid')
 
+
+let buttons = document.querySelectorAll('button')
+    buttons.forEach(button => {
+        button.addEventListener("click", (event) => {
+         let playerSelection = button.id
+         let computerSelection = getComputerChoice()
+         console.log(computerSelection);
+         playRound(playerSelection, computerSelection);
+          
+        })
+      })
+
+    
 function makeGrid( ) {
 
 }
