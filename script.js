68e0c1b8170ae265fa0e06c74e6743c91b82ba51
LeonDEvans <evansleon87@gmail.com>
2024-04-22 21:06:47 -0400

    added a switch statement to the buttons

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-gridSizeButton = document.querySelector('#gridSize')
-eraserButton = document.querySelector('#eraser')
-defaultButton = document.querySelector('#default')
-rainbowButton = document.querySelector('#rainbow')
-monochromeButton = document.querySelector('#monochrome')
-resetButton = document.querySelector('#resetGrid')
+let gridSizeButton = document.querySelector('#gridSize')
+let eraserButton = document.querySelector('#eraser')
+let defaultButton = document.querySelector('#default')
+let rainbowButton = document.querySelector('#rainbow')
+let monochromeButton = document.querySelector('#monochrome')
+let resetButton = document.querySelector('#resetGrid')
 
 
-let buttons = document.querySelectorAll('button')
-    buttons.forEach(button => {
-        button.addEventListener("click", (event) => {
-         let playerSelection = button.id
-         let computerSelection = getComputerChoice()
-         console.log(computerSelection);
-         playRound(playerSelection, computerSelection);
+document.querySelector('button').addEventListener("click", function(event) {
+  // Get the target element that was clicked
+  const clickedElement = event.target;
+
+  // Use a switch statement to handle different cases
+  switch (clickedElement.id) {
+      case "gridSize":
+          console.log('hi')
+        break;
+      case "eraser":
+        break
+      
+      case "default" :
+        break
+
+      case "rainbow":
+        break
+
+      case "monochrome":
+        break
+
+      case "resetGrid":
+        makeGrid();
+        break
           
-        })
-      })
+      default:
+        console.log('something went wrong')
+  }
+          
+});
 
     
 function makeGrid( ) {
